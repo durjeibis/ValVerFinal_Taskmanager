33ff9a54776d646561538a130c49354f49c227bb
durjeibis <durje03@gmail.com>
2014-03-11 20:41:05 -0500

    install gem devise and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Taskmanager::Application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+
+    get "login", to: "devise/sessions#new", as: :login
+    get "logout", to: "devise/sessions#destroy", as: :logout
+    get "register", to: "devise/registrations#new", as: :register
+  end
+
   resources :tasks
 
 
@@ -57,7 +66,7 @@ Taskmanager::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
